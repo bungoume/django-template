8f841ebfb23bf895326a10b0d6eabdcf2962e245
umezaki <bungoume3@yahoo.co.jp>
2015-01-11 00:33:01 +0900

    fix BASE_DIR

--- project_name/project_name/settings/__init__.py
+++ project_name/project_name/settings/__init__.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from os.path import dirname
+BASE_DIR = dirname(dirname(dirname(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
