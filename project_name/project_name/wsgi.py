00422f3ae88ba1e625c85216ae7ab29bb7923427
ume <bungoume3@yahoo.co.jp>
2015-02-10 00:02:45 +0900

    Update wsgi.py

--- project_name/project_name/wsgi.py
+++ project_name/project_name/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
